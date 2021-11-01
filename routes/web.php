2eeab2204665073f007951d793e0a68364ce4b4f
NadaElkholy <nad.c.eng@gmail.com>
2021-11-02 00:45:51 +0200

    Edit route to point to Customer Controller

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [App\Http\Controllers\CustomersController::class, 'index'])->name('customers.index');
